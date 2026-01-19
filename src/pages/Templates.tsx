9cacf63eb5ede4709411849a1a0afde93135bdf4
samihaider12 <samihaider687@gmail.com>
2026-01-20 01:25:46 +0500

    Fix Folder

---
+++ src/pages/Templates.tsx
@@ -0,0 +1,19 @@
+import {Box} from '@mui/material'
+
+import Works from './Works'
+
+ function Templates(){
+return(
+    <>
+<Box>
+    
+<Works />
+
+</Box>
+
+</>
+
+)
+ }
+
+ export default Templates;
