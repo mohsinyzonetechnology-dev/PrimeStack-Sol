698d0470236b8ecd486314662051f6b4de30c05c
samihaider12 <samihaider687@gmail.com>
2026-01-20 00:43:34 +0500

    PS-Dev

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from './theme/theme';
+import AppRouter from './routes/AppRoutes'; // Path check kar lein
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> 
+       
+      <AppRouter />
+    </ThemeProvider>
+  );
+}
+
+export default App;
